54d90bc021985c1d6baf0746a8958e149f08a7cb
ZhaoLei <zhaolei@hacint.com.cn>
2022-10-08 15:48:42 +0800

    hi

---
+++ Server-Browser/slwebsoc.h
@@ -0,0 +1,26 @@
+#ifndef SLWEBSOC_H
+#define SLWEBSOC_H
+#include <QObject>
+#include <QWebSocket>
+#include <functional>
+
+class SLWebsoc  : public QObject
+{
+    Q_OBJECT
+public:
+    SLWebsoc();
+
+    void OpenUrl(const  QUrl& url);
+private:
+    QWebSocket _webSoc;
+    QUrl _url;
+
+private slots:
+    void SlotReceiveMsg(const QString& message);
+    void SlotOnConnect();
+    void SlotDisConnect();
+    void SlotErr(QAbstractSocket::SocketError error);
+    void SlotSSLErr(const QList<QSslError>& errors);
+};
+
+#endif // SLWEBSOC_H
