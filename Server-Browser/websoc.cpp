59835544150c9d56665d739831dcacfb3e754ef5
yuanmo <1445089819@qq.com>
2022-10-10 22:14:37 +0800

    add websocket init

---
+++ Server-Browser/websoc.cpp
@@ -0,0 +1,34 @@
+#include "websoc.h"
+#include "printmsgstation.h"
+
+Websoc::Websoc(PrintMsgStation &printmsgstation)
+    : _print_msg_station(printmsgstation) {
+
+  _webSoc.ignoreSslErrors();
+  connect(&_webSoc, &QWebSocket::connected, this, &Websoc::SlotOnConnect);
+  connect(&_webSoc, &QWebSocket::disconnected, this, &Websoc::SlotDisConnect);
+  connect(&_webSoc,
+          QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::error), this,
+          &Websoc::SlotErr);
+  connect(&_webSoc, &QWebSocket::textMessageReceived, this,
+          &Websoc::SlotReceiveMsg);
+}
+
+void Websoc::OpenUrl(const QUrl &url) {
+  _webSoc.close(QWebSocketProtocol::CloseCodeNormal);
+
+  // if(_webSoc.is)
+}
+
+void Websoc::SlotReceiveMsg(const QString &message) {}
+
+void Websoc::SlotOnConnect() {}
+
+void Websoc::SlotDisConnect() {
+
+  // _webSoc.open(url);
+}
+
+void Websoc::SlotErr(QAbstractSocket::SocketError error) {}
+
+void Websoc::SlotSSLErr(const QList<QSslError> &errors) {}
---
+++ Server-Browser/websoc.h
@@ -0,0 +1,29 @@
+#ifndef SLWEBSOC_H
+#define SLWEBSOC_H
+#include <QObject>
+#include <QWebSocket>
+#include <QWidget>
+#include <functional>
+
+class PrintMsgStation;
+class Websoc : public QObject {
+  Q_OBJECT
+public:
+  Websoc(PrintMsgStation& printmsgstation);
+
+  void OpenUrl(const QUrl &url);
+
+private:
+  QWebSocket _webSoc;
+  QUrl _url;
+  PrintMsgStation& _print_msg_station;
+
+private slots:
+  void SlotReceiveMsg(const QString &message);
+  void SlotOnConnect();
+  void SlotDisConnect();
+  void SlotErr(QAbstractSocket::SocketError error);
+  void SlotSSLErr(const QList<QSslError> &errors);
+};
+
+#endif // SLWEBSOC_H
